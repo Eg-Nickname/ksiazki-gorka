1bea28e9c9ae672aacd2ddc257042546a52d82d3
Jan Lenart <jasiektojo@gmail.com>
2022-10-21 22:07:48 +0200

    Zaczałem robić całość ofert

--- scripts/offer_page_script.js
+++ scripts/offer_page_script.js
@@ -1,8 +1,3 @@
-// import {get_data_for_mainpage} from './script.js';
-// console.log(get_data_for_mainpage);
-/////////////////////////////////////////////////////////////////////////////
-//--------------------------------------------------------------------------
-//Pobieranie elementów do localStorage, skopiowane z innego skryptu, bo eksport nie chiciał działać- Sadge
 const get_data_for_mainpage = function (){
     localStorage.removeItem("books");
     $.ajax({
@@ -29,46 +24,46 @@ const get_data_for_mainpage = function (){
                 local_storage_data=JSON.stringify(local_storage_data);
                 localStorage.setItem("books",local_storage_data);
             }
-            display_sample_offer();
+            display_elements_on_offer_list();
         }
     })
 }
 get_data_for_mainpage();
-//------------------------------------------------------------------------------
-// Pobieranie danych z url i wyświetlenie ich jako przykładowa książka
-const display_sample_offer = function (){
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const bookId=urlParams.get('number');
-    const title = urlParams.get('title').replaceAll("-"," ");
-    const category = urlParams.get('category');
+const display_elements_on_offer_list = function(subject_array=[]){
+    console.log(subject_array);
+    const wrapper=document.querySelector('.books-wrapper')
     const books=JSON.parse(localStorage.getItem('books'));
-    const subject=books[category];
-    if(subject)
-    {
-        const data = {};
-        for(const element of subject){
-            if(element.book_ID==bookId && element.book_name==title){
-                for(const key in element)
-                {
-                    data[key] = element[key];
-                }
-                break;
-            }
+    for(const subject of Object.keys(books)){
+        for(const element of books[subject]){
+            console.log(element);
+            const div=document.createElement('div');
+            $(div).addClass(`subject`);
+            div.setAttribute('id',`${element['category']}${element['book_ID']}`);
+            const img_div=document.createElement('div');
+            $(img_div).addClass(`book-img`);
+            console.log(`"../${element['picture']}"`);
+            $(img_div).css(`background-image`,`url("${element['picture']}")`);
+            const book_name=document.createElement('p');
+            $(book_name).addClass(`book-name`);
+            $(book_name).html(`${element['book_name']}`);
+            const book_price=document.createElement('p');
+            $(book_price).addClass(`book-price`);
+            $(book_price).html(`Zaczyna się od 30 PLN`); //dodać najniższą cenę to bazy
+            const button=document.createElement('button');
+            $(button).addClass(`book-btn`);
+            button.setAttribute('id',`book-btn${element['book_ID']}`);
+            $(button).html('Sprawdź ofertę');
+            div.append(img_div,book_name,book_price,button);
+            $('.books-wrapper').append(div);
         }
-        console.log(data);
-        if(Object.keys(data).length!=0){
-            console.log(data.picture);
-            $('body').css('background-image',`url("${data.picture}")`);
-            // `url("${element['picture']}")`
-        }
-        else{
-            $('body').append('<h1>Strona nie istnieje</h1>')
-        }
-    }
-    else{
-        $('body').append('<h1>Strona nie istnieje</h1>')
     }
 }
-//--------------------------------------------------------------------------------
-////////////////////////////////////////////////////////////////////////////////////
+$('#sumbit_filters').on('click', function(){
+    const checked_inputs=document.querySelectorAll('.subject_filter:checked');
+    console.log(checked_inputs);
+    const subject_array=[];
+    for(const element of checked_inputs){
+        subject_array.push(element.id);
+    }
+    display_elements_on_offer_list(subject_array);
+});
