ffb895579c33a100f054403abb57f9f54d62d0ab
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2022-10-22 12:34:24 +0200

    error spany

--- login_page.php
+++ login_page.php
@@ -52,18 +52,21 @@ if(isset($_SESSION['logged_in']))
                             <div class="input-field">
                             <input type="text" placeholder="Wpisz imię" name="name" id="name" value="siema">
                             </div>
+                            <span class="error_log_info">Lorem Ipsum</span>
                         </div>
                         <div class="label-wrapper sign-up-form">
                             <label for="surname">Nazwisko:</label>
                             <div class="input-field">
                             <input type="text" placeholder="Wpisz nazwisko" name="surname" id="surname" value="si"/>
                             </div>
+                            <span class="error_log_info">Lorem Ipsum</span>
                         </div>
                         <div class="label-wrapper sign-up-form">
                             <label for="username">Username:</label>
                             <div class="input-field">
                             <input type="text" placeholder="Wpisz nazwę użytkownika" name="username" id="username" value="2s"/>
                             </div>
+                            <span class="error_log_info">Lorem Ipsum</span>
                         </div>
                         <button class="register-in-btn" id="register">Stwórz konto</button> 
                     </form>
@@ -74,12 +77,14 @@ if(isset($_SESSION['logged_in']))
                             <div class="input-field">
                             <input type="email" placeholder="adres email" name="email" id="email" />
                             </div>
+                            <span class="error_log_info_login">Lorem Ipsum</span> 
                         </div>
                         <div class="label-wrapper sign-in-form">
                             <label for="password">Hasło:</label>
                             <div class="input-field">
                                 <input type="password" placeholder="Wpisz hasło" id="password" name="password" />
                             </div> 
+                            <span class="error_log_info_login">Lorem Ipsum</span> 
                         </div>
                         <button class="sign-in-btn" id="log_in">Zaloguj się</button>
                         <!-- <button class="sign-in-btn-google" id="log_in_google"><span class="icon"></span>Zaloguj się za pomocą Google</button> -->
@@ -97,24 +102,28 @@ if(isset($_SESSION['logged_in']))
                             <div class="input-field">
                             <input type="email" placeholder="Wpisz swoj email" name="register_email" id="register_email" value="12@df.pl" />
                             </div>
+                            <span class="error_log_info">Lorem Ipsum</span>
                         </div>
                         <div class="label-wrapper register-in-form">
                             <label for="register_password">Hasło:</label>
                             <div class="input-field">
                                 <input type="password" placeholder="Hasło musi zawierać 8 znaków" name="register_password" id="register_password" value="123456789" />
-                            </div> 
+                            </div>
+                            <span class="error_log_info">Lorem Ipsum</span> 
                         </div>
                         <div class="label-wrapper register-in-form">
                             <label for="check_password">Hasło:</label>
                             <div class="input-field">
                                 <input type="password" placeholder="Potwierdź hasło"  name="check_password" id="check_password" value="123456789" />
-                            </div> 
+                            </div>
+                            <span class="error_log_info">Lorem Ipsum</span> 
                         </div>
                         <button class="register-in-btn-google" id="register_google"><span class="icon"></span><span id="google-text">Zarejestruj się z Google</span></button>
                     </form>
                 </div>  
             </div>
         </div>
+        <span class="error_log_info_password">Lorem Ipsum twoje konto srututut wymagan nie spelnia</span>
         <span class="sign-up-goto-span form--hidden"><p id="sign-up-goto">Nie masz jeszcze konta? &nbsp;<a href="#" id="linkCreateAccount"> Zarejestruj się</a></p></span>
         <span class="sign-in-goto-span"><p id="sign-in-goto"> Masz już konto? &nbsp;<a href="#" id="linkLogin"> Zaloguj się</a></p></span>
     </div>  
